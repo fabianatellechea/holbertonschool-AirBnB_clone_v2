64f01da8412d2e6efdb7f632fb32bcd3ae505872
Fabiana Tellechea <85513647+fabianatellechea@users.noreply.github.com>
2023-11-29 20:54:10 +0000

    added: user alchemy

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(string(123), nullable=False)
+    password = Column(string(123), nullable=False)
+    first_name = Column(string(123), nullable=False)
+    last_name = Column(string(123), nullable=False)
